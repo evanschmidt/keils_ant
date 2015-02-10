f724c25056d83f4cda1a7ed301a7bdbe93f17d25
schmook <eschmidt@vdesign.com>
2015-02-10 16:46:26 -0600

    adding print template

---
+++ templates/print.tpl.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file
+ * Default print module template
+ *
+ * @ingroup print
+ */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $print['language']; ?>" xml:lang="<?php print $print['language']; ?>">
+  <head>
+    <?php print $print['head']; ?>
+    <?php print $print['base_href']; ?>
+    <title><?php print $print['title']; ?></title>
+    <?php print $print['scripts']; ?>
+    <?php print $print['sendtoprinter']; ?>
+    <?php print $print['robots_meta']; ?>
+    <?php print $print['favicon']; ?>
+    <?php print $print['css']; ?>
+  </head>
+  <body>
+    <div class="print-logo"><?php print $print['logo']; ?></div>
+	<?php
+      $image_uri = $node->field_antique_images['und'][0]['uri'];
+      $image_url_with_style = image_style_url('antique_print',$image_uri);
+      echo '<img src="'.$image_url_with_style.'">';
+	?>
+    <div class="print-content"><?php print $print['content']; ?></div>
+    <div class="print-footer"><?php print $print['footer_message']; ?></div>
+    <?php print $print['footer_scripts']; ?>
+  </body>
+</html>
